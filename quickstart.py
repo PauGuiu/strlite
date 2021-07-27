1100a4b485aba19a0f29de87fc7232710ed6f32f
PauGuiu <34050433+PauGuiu@users.noreply.github.com>
2021-07-27 16:17:39 -0500

    Add files via upload

---
+++ quickstart.py
@@ -0,0 +1,47 @@
+
+# -----------------------------------------------------------
+import streamlit as st
+import pandas as pd
+# -----------------------------------------------------------
+
+# Helper functions
+# -----------------------------------------------------------
+# Load data from external source
+df = pd.read_csv(
+    "https://raw.githubusercontent.com/ThuwarakeshM/PracticalML-KMeans-Election/master/voters_demo_sample.csv"
+)
+# -----------------------------------------------------------
+
+# Sidebar
+# -----------------------------------------------------------
+
+# -----------------------------------------------------------
+
+
+# Main
+# -----------------------------------------------------------
+# Create a title for your app
+st.title("Interactive K-Means Clustering")
+
+# A description
+st.write("Here is the dataset used in this analysis:")
+
+# Display the dataframe
+st.write(df)
+# ------------------------------
+# Display the dataframe
+df_display = st.checkbox("Display Raw Data", value=True)
+
+if df_display:
+    st.write(df)
+# SIDEBAR
+# -----------------------------------------------------------
+sidebar = st.sidebar
+df_display = sidebar.checkbox("Display Raw Data", value=True)
+
+n_clusters = sidebar.slider(
+    "Select Number of Clusters",
+    min_value=2,
+    max_value=10,
+)
+# -----------------------------------------------------------
